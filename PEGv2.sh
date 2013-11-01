fe06583eaade9fee64e06ec01500342a6ce5714e
ishwon <ish@hacklog.in>
2013-11-01 06:45:53 -0700

    Update PEGv2.sh

--- PEGv2.sh
+++ PEGv2.sh
@@ -8,6 +8,31 @@
 # License details at http://creativecommons.org/licenses/by/3.0.
 #
 
+ROOT_UID=0
+E_NOTROOT=87
+ARCH_TEST=`uname -i`
+ARCH_TYPE=x86_64
+
+if [ "$UID" -ne "$ROOT_UID" ]
+then
+    echo "You must run this script as root."
+    exit $E_NOTROOT
+fi
+
+if [ -e /etc/SuSE-release ]
+then
+    echo "Woohooo! You're on SUSE."
+        if [ $ARCH_TEST == $ARCH_TYPE ]
+        then
+            echo "Awesome! You're on 64bit :)"
+        else
+            echo "Ahaan! I cannot run on non 64bit machines. Sorry"
+        fi
+else
+    echo "Oops! That's not a SUSE machine."
+    exit
+fi
+
 echo "Adding HACKLOG.in repository"
 echo "..."
 zypper ar http://lab.hacklog.in/openSUSE_13.1/repo HACKLOG.in
